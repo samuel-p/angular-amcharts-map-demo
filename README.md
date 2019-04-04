05bf5766471e4fe4e49ae422b826de5d5b9426a1
Samuel Philipp <sphilipp@av-test.de>
2019-04-04 09:31:09 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MyProject
+# angular-amcharts-map-demo
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-project'`, () => {
+  it(`should have as title 'angular-amcharts-map-demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-project');
+    expect(app.title).toEqual('angular-amcharts-map-demo');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-project!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-amcharts-map-demo!');
   });
 });
